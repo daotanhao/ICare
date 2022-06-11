8921adbabe02f4c5861f219cdec066ef9d8c5ea6
huyy802 <20521401@gm.uit.edu.vn>
2022-06-11 23:24:45 +0700

    feature - sleep, water

--- app/src/main/java/com/icare/SignInActivity.java
+++ app/src/main/java/com/icare/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.icare;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,26 +10,55 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.icare.MainActivity;
 import com.icare.SignUpActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 public class SignInActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 120;
     private EditText txtEmail, txtPassword;
     private Button btnSignIn, btnSignUp, btnForgot;
     private Button btnGoogle, btnFacebook;
@@ -36,14 +66,31 @@ public class SignInActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private static final String tempEmail = "tempEmail";
+    private FirebaseFirestore firestore;
+    private DocumentReference docRef;
+    CallbackManager mCallbackManager;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions gso;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         addControls();
         addEvents();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Intent INTENT = new Intent(Intent.ACTION_MAIN);
+        INTENT.addCategory(Intent.CATEGORY_HOME);
+        INTENT.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(INTENT);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -64,8 +111,127 @@ public class SignInActivity extends AppCompatActivity {
                                      }
         );
         SignUp();
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+                progressBar.setVisibility(View.VISIBLE);
 
+            }
+        });
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(SignInActivity.this, Arrays.asList("email", "public_profile"));
+                LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        handleFacebookAccessToken(loginResult.getAccessToken());
+                        progressBar.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+
+                    }
+                });
+            }
+        });
+        btnForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, ForgotPasswordActivity.class));
+                finish();
+            }
+        });
     }
+
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            result();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    private void result() {
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                String email, name;
+                try {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    email = object.getString("email");
+                    name = object.getString("name");
+
+                    SharedPreferences sharedPreferences = getSharedPreferences(tempEmail, MODE_PRIVATE);
+
+                    firestore = FirebaseFirestore.getInstance();
+                    docRef = firestore.collection("users").document(email);
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @RequiresApi(api = Build.VERSION_CODES.O)
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    Toast.makeText(SignInActivity.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    CreateUserOnFirebase(email, name);
+                                    Toast.makeText(SignInActivity.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(SignInActivity.this, CompleteUserInfoActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+
+                                SharedPreferences.Editor editor;
+                                editor = sharedPreferences.edit();
+                                editor.putString("Email", email);
+                                editor.apply();
+                            }
+                        }
+                    });
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fields", "email , name");
+        request.setParameters(bundle);
+        request.executeAsync();
+    }
+
+    private void signInWithGoogle() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 
     private void SignUp() {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
@@ -127,6 +293,131 @@ public class SignInActivity extends AppCompatActivity {
         btnForgot = (Button) findViewById(R.id.btnForgot);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         btnFacebook = (Button) findViewById(R.id.btnFacebook);
-        btnGoogle = (MaterialButton) findViewById(R.id.btnGoogle);
+        btnGoogle = (Button) findViewById(R.id.btGoogle);
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(com.firebase.ui.auth.R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //Facebook
+        mCallbackManager = CallbackManager.Factory.create();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Exception exception = task.getException();
+            if (task.isSuccessful()) {
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    Log.d("SigninActivity", "firebaseAuthWithGoogle:" + account.getId());
+                    firebaseAuthWithGoogle(account.getIdToken());
+                    SharedPreferences sharedPreferences = getSharedPreferences(
+                            tempEmail, MODE_PRIVATE);
+
+                    GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+                    if (acct != null) {
+                        String googleEmail = acct.getEmail();
+                        String userName = acct.getDisplayName();
+
+                        firestore = FirebaseFirestore.getInstance();
+                        docRef = firestore.collection("users").document(googleEmail);
+                        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @RequiresApi(api = Build.VERSION_CODES.O)
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        Toast.makeText(SignInActivity.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        CreateUserOnFirebase(googleEmail, userName);
+                                        Toast.makeText(SignInActivity.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(SignInActivity.this, CompleteUserInfoActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    finish();
+                                }
+                            }
+                        });
+
+
+                        //set up shareRef
+                        SharedPreferences.Editor editor;
+                        editor = sharedPreferences.edit();
+                        editor.putString("Email", googleEmail);
+                        editor.apply();
+                    }
+                } catch (ApiException e) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("SigninActivity", "Google sign in failed", e);
+                }
+            } else {
+                Log.w("SigninActivity", exception.toString());
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void CreateUserOnFirebase(String googleEmail, String userName) {
+        firestore = FirebaseFirestore.getInstance();
+        LocalDate today = LocalDate.now();
+
+        // Save user data to firestore
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", userName);
+        user.put("email", googleEmail);
+        user.put("gender", "empty");
+        user.put("date_of_birth", "empty");
+        user.put("join_date", today.toString());
+        user.put("weight", "empty");
+        user.put("height", "empty");
+        user.put("step_goal", "empty");
+        user.put("drink_goal", "empty");
+        user.put("calories_burn_goal", "empty");
+        user.put("sleep_goal", "empty");
+        user.put("on_screen_goal", "empty");
+        user.put("recent_workout", "empty");
+        user.put("time_to_sleep", "empty");
+        firestore.collection("users").document(googleEmail)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignInActivity.this, "Fail to save data to Firestore", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SigninActivity", "signInWithCredential:success");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("SigninActivity", "signInWithCredential:failure", task.getException());
+                        }
+                    }
+                });
     }
 }
